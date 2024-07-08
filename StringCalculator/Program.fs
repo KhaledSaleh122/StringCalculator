28d95279b15f5c5cab4f7d8255b2aa37be694d6d
khaled Saleh <voxagolde2003@gmail.com>
2024-07-08 23:28:10 +0300

    Support multiple delimiters

--- StringCalculator/Program.fs
+++ StringCalculator/Program.fs
@@ -1,6 +1,9 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
 try
     StringCalculator.Add "1,2,3,1001" |> printfn "Sum: %i"
+    StringCalculator.Add "//;\n1;2,3,1001" |> printfn "Sum: %i"
+    StringCalculator.Add "//[;]\n1;2,3,1001" |> printfn "Sum: %i"
+    StringCalculator.Add "//[;][pp]\n1;2pp3,1001" |> printfn "Sum: %i"
 with
 | StringCalculator.NegativesNoAllowed negs ->
     printfn "Negatives not allowed: %A" negs
--- StringCalculator/StringCalculator.fs
+++ StringCalculator/StringCalculator.fs
@@ -2,7 +2,13 @@
 
 exception NegativesNoAllowed of int[]
 let Add (numbers : string) = 
-    let delimiters = [|",";"\n"|]
+    let mutable delimiters = [|",";"\n"|]
+    let mutable numbers = numbers
+    if numbers.StartsWith("//") then
+        let inputs = numbers.Split([|'\n'|],2)
+        let customDelimiters = inputs.[0].Split([|"["; "]"; "/"|],System.StringSplitOptions.RemoveEmptyEntries)
+        delimiters <- Array.append delimiters customDelimiters
+        numbers <- inputs.[1]
     let numbers = numbers.Split(delimiters, System.StringSplitOptions.RemoveEmptyEntries)
                   |> Array.map int
     let negativeNumbers = numbers |> Array.filter(fun n -> n < 0)
