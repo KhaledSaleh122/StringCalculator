e8c60fce7684bb35d3228723501a28e9bf5eeeee
khaled Saleh <voxagolde2003@gmail.com>
2024-07-08 22:54:03 +0300

    Sum the numbers based on default delimiters

--- StringCalculator/StringCalculator.fs
+++ StringCalculator/StringCalculator.fs
@@ -1,2 +1,7 @@
 ﻿module StringCalculator
 
+let Add (numbers : string) = 
+    let delimiters = [|",";"\n"|]
+    numbers.Split(delimiters, System.StringSplitOptions.RemoveEmptyEntries)
+    |> Array.map int
+    |> Array.sum
